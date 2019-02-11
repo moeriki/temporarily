915c6fa3d1500484ee06f9f88bf2c0c303698999
Dieter Luypaert <dieterluypaert@gmail.com>
2019-02-11 14:27:08 +0100

    Bump dependencies

--- .eslintrc.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  extends: ['airbnb-base', 'prettier'],
-  parserOptions: {
-    sourceType: 'common-js',
-  },
-  root: true,
-  rules: { 'no-sync': 0 },
-};
--- prettier.config.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = {
-  arrowParens: 'all',
-  singleQuote: true,
-  trailingComma: 'all',
-};
